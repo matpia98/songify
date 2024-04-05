0b8c22a825ea097522e829d1065437cd6adc5d2c
Mateusz <matpia98@gmail.com>
2024-04-05 20:15:59 +0200

    added dto records, added 3 getMapping methods in SongsController

---
+++ src/main/java/com/songify/SongResponseDto.java
@@ -0,0 +1,5 @@
+package com.songify;
+
+import java.util.Map;
+
+public record SongResponseDto(Map<Integer, String> songs) {}
---
+++ src/main/java/com/songify/SongsController.java
@@ -0,0 +1,49 @@
+package com.songify;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@Log4j2
+public class SongsController {
+
+    Map<Integer, String> database = new HashMap<>();
+
+    @GetMapping("/songs")
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit) {
+        database.put(1, "shawnmendes song1");
+        database.put(2, "ariana grande song2");
+        database.put(3, "ariana grande song22121");
+        database.put(4, "ariana grande song2another");
+        if (limit != null) {
+            Map<Integer, String> limitedMap = database.entrySet()
+                    .stream()
+                    .limit(limit)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            SongResponseDto response = new SongResponseDto(limitedMap);
+            return ResponseEntity.ok(response);
+        }
+
+        SongResponseDto response = new SongResponseDto(database);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<SingleSongResponseDto> getSongById(
+            @PathVariable Integer id,
+            @RequestHeader(required = false) String requestId) {
+        log.info(requestId);
+        String song = database.get(id);
+        if (song == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        SingleSongResponseDto response = new SingleSongResponseDto(song);
+        return ResponseEntity.ok(response);
+    }
+}
