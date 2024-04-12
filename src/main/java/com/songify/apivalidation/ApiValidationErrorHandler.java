617354fe6eb828cf1f64c060561d98ce267bd447
Mateusz <matpia98@gmail.com>
2024-04-12 14:25:20 +0200

    api error handling refactor

--- src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
+++ src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
@@ -2,25 +2,24 @@ package com.songify.apivalidation;
 
 import com.songify.song.infrastructure.controller.SongRestController;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice(assignableTypes = SongRestController.class)
 public class ApiValidationErrorHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiValidationErrorResponseDto handleValidationException(MethodArgumentNotValidException exception){
+    public ResponseEntity<ApiValidationErrorResponseDto> handleValidationException(MethodArgumentNotValidException exception){
         List<String> messages = getErrorsFromException(exception);
-        return new ApiValidationErrorResponseDto(messages);
+        ApiValidationErrorResponseDto response = new ApiValidationErrorResponseDto(messages, BAD_REQUEST);
+        return ResponseEntity.status(BAD_REQUEST).body(response);
     }
 
     private List<String> getErrorsFromException(MethodArgumentNotValidException exception) {
--- src/main/java/com/songify/apivalidation/ApiValidationErrorResponseDto.java
+++ src/main/java/com/songify/apivalidation/ApiValidationErrorResponseDto.java
@@ -1,7 +1,9 @@
 package com.songify.apivalidation;
 
+import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
-public record ApiValidationErrorResponseDto(List<String> errors) {
+public record ApiValidationErrorResponseDto(List<String> errors, HttpStatus status) {
 
 }
