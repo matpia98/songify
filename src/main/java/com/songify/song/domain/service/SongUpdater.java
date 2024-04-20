927e5161626cb090e1ce00a5dd70e9eb914a81ae
Mateusz <matpia98@gmail.com>
2024-04-20 20:12:54 +0200

    added method in SongUpdater that shows how transactions work, optimized imports

--- src/main/java/com/songify/song/domain/service/SongUpdater.java
+++ src/main/java/com/songify/song/domain/service/SongUpdater.java
@@ -3,26 +3,35 @@ package com.songify.song.domain.service;
 import com.songify.song.domain.model.Song;
 import com.songify.song.domain.repository.SongRepository;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 @Transactional
+@AllArgsConstructor
 public class SongUpdater {
 
     private final SongRepository songRepository;
     private final SongRetriever songRetriever;
+    private final SongAdder songAdder;
 
-    SongUpdater(SongRepository songRepository, SongRetriever songRetriever) {
-        this.songRepository = songRepository;
-        this.songRetriever = songRetriever;
-    }
 
     public void updateById(Long id, Song newSong) {
         songRetriever.existsById(id);
         songRepository.updateById(id, newSong);
     }
+
+    public void someComplicatedLogic(){
+        songRepository.updateById(1L, new Song("siema", "siema"));
+        songRepository.updateById(2L, new Song("siema", "siema"));
+        songRetriever.existsById(1000L);
+        songRepository.updateById(3L, new Song("siema", "siema"));
+        Song piesSong = songAdder.addSong(new Song("pies", "pies"));
+        songRepository.updateById(piesSong.getId(), new Song("pies2", "pies2"));
+    }
+
 
     public Song updatePartiallyById(Long id, Song songFromRequest) {
         Song songFromDatabase = songRetriever.findSongById(id);
