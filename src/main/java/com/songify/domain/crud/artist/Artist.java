c678498e4e48d8a3685f68c9a7fbfd445235469b
Mateusz <matpia98@gmail.com>
2024-04-24 19:05:23 +0200

    artist entity and migration

--- src/main/java/com/songify/domain/crud/artist/Artist.java
+++ src/main/java/com/songify/domain/crud/artist/Artist.java
@@ -1,4 +1,33 @@
 package com.songify.domain.crud.artist;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter(AccessLevel.PACKAGE)
+@Setter(AccessLevel.PACKAGE)
 class Artist {
+
+    @Id
+    @GeneratedValue(generator = "artist_id_seq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(
+            name = "artist_id_seq",
+            sequenceName = "artist_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+
 }
