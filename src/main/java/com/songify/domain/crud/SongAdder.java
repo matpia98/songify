a5494021ea6108b079eeb31e67dd854fd2d99ce6
Mateusz <matpia98@gmail.com>
2024-04-30 09:47:40 +0200

    genre in songdto, new migration with new genres added

--- src/main/java/com/songify/domain/crud/SongAdder.java
+++ src/main/java/com/songify/domain/crud/SongAdder.java
@@ -1,5 +1,6 @@
 package com.songify.domain.crud;
 
+import com.songify.domain.crud.dto.GenreDto;
 import com.songify.domain.crud.dto.SongDto;
 import com.songify.domain.crud.dto.SongLanguageDto;
 import com.songify.domain.crud.dto.SongRequestDto;
@@ -24,7 +25,7 @@ class SongAdder {
         Song song = new Song(songDto.name(), songDto.releaseDate(), songDto.duration(), songLanguage);
         log.info("adding new song: " + song);
         Song save = songRepository.save(song);
-        return new SongDto(save.getId(), save.getName());
+        return new SongDto(save.getId(), save.getName(), new GenreDto(save.getGenre().getId(), save.getGenre().getName()));
     }
 
 
