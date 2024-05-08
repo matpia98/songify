501536550c610efaa514e3ee600cf2b56f0b1e14
Mateusz <matpia98@gmail.com>
2024-05-08 12:09:42 +0200

    testcontainers, connection to database by testcontainers, overriding app properties

--- src/main/java/com/songify/domain/songplayer/YoutubeHttpClient.java
+++ src/main/java/com/songify/domain/songplayer/YoutubeHttpClient.java
@@ -1,5 +1,8 @@
 package com.songify.domain.songplayer;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 interface YoutubeHttpClient {
     String playSongByName(String name);
 }
